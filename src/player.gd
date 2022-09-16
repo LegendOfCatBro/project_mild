05eab85e83a4d73eae32c52a8cdd6a00168e5702
Eric <torracat33@gmail.com>
2022-09-16 15:18:16 -0700

    Edited player, added tilemap
    Replaced test platform with a tilemap that I used to make a small test level.
    KinematicBody2D renamed to player

--- src/player.gd
+++ src/player.gd
@@ -9,12 +9,24 @@ func get_input():
 	velocity.x = 0 
 	if Input.is_action_pressed("right"):
 		velocity.x += run_power
+		get_node("Sprite").flip_h = false
 	if Input.is_action_pressed("left"):
 		velocity.x -= run_power
+		get_node("Sprite").flip_h = true
 	#make it so its a jump and not infinite moon jump
 	if Input.is_action_pressed("up") and is_on_floor():
 		velocity.y = jump_power
-
+		
+	""" This is the old way I did the flipping of the sprite
+		I'm pretty sure it is less efficient so I commented it out but 
+		I don't want to forget that we could use this in the future for detecting
+		direction 
+	var direction = int(velocity.direction_to(Vector2.LEFT).x)
+	if direction > 0:
+		get_node("Sprite").flip_h = truea
+	elif direction < 0:
+		get_node("Sprite").flip_h = false
+	"""
 func _physics_process(delta):
 	get_input()
 	velocity.y += gravity
