7369db947c6e2a550b6b2b465b544157935879d0
Eric <torracat33@gmail.com>
2022-09-16 09:25:15 -0700

    Rewrote movement scriptaa

--- src/KinematicBody2D.gd
+++ src/KinematicBody2D.gd
@@ -1,25 +1,22 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
-
+export (int) var run_power = 500
+export (int) var jump_power = -1000
+export (int) var gravity = 50
 var velocity = Vector2()
 
-const GRAVITY = 1
-
 func get_input():
-	velocity = Vector2()
+	velocity.x = 0 
 	if Input.is_action_pressed("right"):
-		velocity.x = 10
+		velocity.x += run_power
 	if Input.is_action_pressed("left"):
-		velocity.x -= 10
-	if Input.is_action_pressed("down"):
-		velocity.y = 10
-	if Input.is_action_pressed("up"):
-		velocity.y -= 10
-	velocity.y += GRAVITY
-	
-	velocity = velocity.normalized() * speed
+		velocity.x -= run_power
+	#make it so its a jump and not infinite moon jump
+	if Input.is_action_pressed("up") and is_on_floor():
+		velocity.y = jump_power
 
 func _physics_process(delta):
 	get_input()
-	velocity = move_and_slide(velocity)
+	velocity.y += gravity
+	#added this second parameter so it knows which way is up when checking for floor
+	velocity = move_and_slide(velocity, Vector2.UP) 
