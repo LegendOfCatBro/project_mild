0d4a6d58decac0ccac3fa3d76c5f9fd6b3784aef
Eric <torracat33@gmail.com>
2022-09-16 21:42:47 -0700

    movement script adjustments, bugfix
    replacing old bugs with new ones lets go

--- src/player.gd
+++ src/player.gd
@@ -3,10 +3,11 @@ extends KinematicBody2D
 export (int) var run_power = 500
 export (int) var jump_power = -1000
 export (int) var gravity = 50
+export (int) var friction = 1.5
 var velocity = Vector2()
 
 func get_input():
-	velocity.x = 0 
+	
 	if Input.is_action_pressed("right"):
 		velocity.x += run_power
 		get_node("Sprite").flip_h = false
@@ -14,9 +15,15 @@ func get_input():
 		velocity.x -= run_power
 		get_node("Sprite").flip_h = true
 	#make it so its a jump and not infinite moon jump
-	if Input.is_action_pressed("up") and is_on_floor():
-		velocity.y = jump_power
-		
+	if is_on_floor():
+		if Input.is_action_pressed("up"):
+			velocity.y = jump_power
+		else:
+			velocity.y = 0
+	else:
+		velocity.y += gravity
+	velocity.x /= friction
+	
 	""" This is the old way I did the flipping of the sprite
 		I'm pretty sure it is less efficient so I commented it out but 
 		I don't want to forget that we could use this in the future for detecting
@@ -29,6 +36,6 @@ func get_input():
 	"""
 func _physics_process(delta):
 	get_input()
-	velocity.y += gravity
+	
 	#added this second parameter so it knows which way is up when checking for floor
 	velocity = move_and_slide(velocity, Vector2.UP) 
