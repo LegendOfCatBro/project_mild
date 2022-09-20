1b16387a21ac1a08345e58d36e284dd7cbc69a76
Eric <torracat33@gmail.com>
2022-09-19 21:51:41 -0700

    animations! bugfixes
    replaced my guy with animated walk cycle, fixed some bugs, expanded map
    slopes work right now lets gooo

--- src/player.gd
+++ src/player.gd
@@ -1,27 +1,29 @@
 extends KinematicBody2D
 
-export (int) var run_power = 500
+export (int) var run_power = 300
 export (int) var jump_power = -1000
 export (int) var gravity = 50
 export (int) var friction = 1.5
 var velocity = Vector2()
+var snap = Vector2(0,32)
+onready var sprite = get_node("AnimatedSprite")
 
 func get_input():
-	
 	if Input.is_action_pressed("right"):
 		velocity.x += run_power
-		get_node("Sprite").flip_h = false
+		sprite.flip_h = false
 	if Input.is_action_pressed("left"):
 		velocity.x -= run_power
-		get_node("Sprite").flip_h = true
-	#make it so its a jump and not infinite moon jump
-	if is_on_floor():
-		if Input.is_action_pressed("up"):
-			velocity.y = jump_power
-		else:
-			velocity.y = 0
+		sprite.flip_h = true
+	if Input.is_action_just_pressed("left") or Input.is_action_just_pressed("right"):
+		sprite.play("walk")
+	#this is shitty, optimize it later!!!
+	if is_on_floor() and Input.is_action_pressed("up"):
+		velocity.y = jump_power
+		snap = Vector2.ZERO
 	else:
 		velocity.y += gravity
+		snap = Vector2(0,32)
 	velocity.x /= friction
 	
 	""" This is the old way I did the flipping of the sprite
@@ -36,6 +38,8 @@ func get_input():
 	"""
 func _physics_process(delta):
 	get_input()
-	
-	#added this second parameter so it knows which way is up when checking for floor
-	velocity = move_and_slide(velocity, Vector2.UP) 
+	if abs(velocity.x) < 1 and sprite.is_playing():
+		sprite.stop()
+		sprite.set_frame(0)
+	#why does it still process x movement even with the .y???
+	velocity.y = move_and_slide_with_snap(velocity, snap, Vector2.UP, true).y 
